0d647ed5eefdbc1da0a3221609b9bb2a18594a89
richKirl <162905102+richKirl@users.noreply.github.com>
2025-10-09 07:52:00 +1030

    Delete testFolder directory

--- testFolder/compile.sh
+++
@@ -1,6 +0,0 @@
-g++-14 \
- -std=c++26 -O3 -msse4.2 -mavx \
- main.cpp Window.cpp CubeV.cpp Object3D.cpp Shader.cpp Font.cpp \
- -o \
- testcolBB \
- -DSHM -I/usr/include/freetype2/ -lGL -lGLU -lGLEW -lglfw -lm -lfreetype
